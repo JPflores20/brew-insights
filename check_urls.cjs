417841affbc52a399d6a3d7c10decc85e16a1eb4
JPflores20 <pepe.jlfc.16@gmail.com>
2026-06-03 14:30:12 -0600

    .

--- check_urls.cjs
+++
@@ -1,7 +0,0 @@
-const https = require('https');
-const files = ['S2600018', 'S2600019', 'S2600020', 'S2600021'];
-files.forEach(f => {
-  https.get(`https://brew-insights.web.app/Datos%20semanales/${f}.DBF`, res => {
-    console.log(`${f}: ${res.statusCode} ${res.headers['content-type']}`);
-  });
-});
